7347526116daee1347f619280c4d3ce5be0c06c9
farihaanwer <farihaanwer15@>
2022-02-08 15:58:05 +0500

    .

--- src/app/modules/core/components/side-nav/side-nav.component.spec.ts
+++ src/app/modules/core/components/side-nav/side-nav.component.spec.ts
@@ -6,7 +6,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 import { SideNavComponent } from './side-nav.component';
 
 describe('SideNavComponent', () => {
--- src/app/modules/core/components/side-nav/side-nav.component.ts
+++ src/app/modules/core/components/side-nav/side-nav.component.ts
@@ -6,7 +6,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-side-nav',
-  templateUrl: './side-nav.component.html',
+  templateUrl:'./side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent {
